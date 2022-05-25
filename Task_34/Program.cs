a7b1d2f3fa9aeb868abec5bb1697cdf04aa2a1b0
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 20:07:45 +0300

    Added Task 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+// [345, 897, 568, 234] -> 2
+bool run = true;
+while (run)
+{
+    Console.Clear();
+    Console.WriteLine("Укажите размер массива");
+    int size = int.Parse(Console.ReadLine());
+    int[] array = new int[size];
+    int count = 0;
+    var newRnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = newRnd.Next(100, 1000);
+
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count += 1;
+    }
+    Console.WriteLine(count);
+
+    Console.WriteLine("Попробуем ещё раз? y/n");
+    run = Console.ReadKey().Key == ConsoleKey.Y;
+}
