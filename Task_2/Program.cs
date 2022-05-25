be5cc11bf9468bd7b88c734be7f7186ff21907af
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 19:43:39 +0300

    Added Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход
+//принимает два числа и выдаёт, 
+//какое число большее, а
+//какое меньшее.
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
