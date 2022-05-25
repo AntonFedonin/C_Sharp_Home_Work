86597ca28450c3ebdb1edacdd53ffdd83341b488
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 19:59:48 +0300

    Added Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// string str = n.ToString();
+string n = (Console.ReadLine());
+if (n <= "99" )
+    Console.WriteLine("Нет третьей цифры");
+else
+    Console.WriteLine(str[2]);
