d95c32a3b99a8e691669e04bc30f731a84e46e87
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 19:54:47 +0300

    Added Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+//int count = 2;
+for (int i = 2; i < a; i+=2)
+{
+    Console.Write(i+" ");
+}       
+            
+//Console.WriteLine(count);
+//while(count < a)
+//{
+  //   Console.WriteLine(count);
+    // count = count + 2;
+    //}
+    
+
+//for (int count = 1; count < a; count++)       
+//if (count%2==0)            
+
+
+
+
+
+
