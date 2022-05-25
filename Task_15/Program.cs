c95bd413a843d47134601b017c790e9e67552d84
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 20:00:53 +0300

    Added Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 7 || n < 1)
+    Console.WriteLine("Ошибка ввода");
+else
+    Console.WriteLine(n < 6 ? $"Нет" : "Да");
