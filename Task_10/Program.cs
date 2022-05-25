d1726c3743ad1b51c1ed9afd8166e69210777641
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 19:56:58 +0300

    Added Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число -> ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(n % 100 / 10);
+
+// int[] array = new int[3];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = int.Parse(Console.ReadLine()); 
+// }
+// Console.WriteLine(array[1]);
