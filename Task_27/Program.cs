6bf30b6992b1ece9e7420fd0495954967fa84177
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-25 20:05:50 +0300

    Added Task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int digit = 0;
+while (n > 0)
+{
+    digit = n % 10;
+    sum = sum + digit;
+    n = n / 10;
+}
+Console.WriteLine($"Сумма чисел равна {sum}");
+
