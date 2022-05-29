00378f793a70f00b0444628d3d519be7ddc32325
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-05-29 14:58:04 +0300

    Complete Task 43

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -4,10 +4,10 @@
 
 Console.Clear();
 Console.WriteLine("Введите число: ");
-// int n = int.Parse(Console.ReadLine());
-// string str = n.ToString();
-string n = (Console.ReadLine());
-if (n <= "99" )
+int n = int.Parse(Console.ReadLine());
+string str = n.ToString();
+// string n = (Console.ReadLine());
+if (n <= 99)
     Console.WriteLine("Нет третьей цифры");
 else
     Console.WriteLine(str[2]);
--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 43. Напишите программу, которая найдёт точку
-// пересечения двух прямых, заданных уравнениями y = k1 *
-// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 // пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 =
 // 9 -> (-0,5; 5,5)
@@ -16,11 +16,12 @@ while (run)
     double b2 = Convert.ToDouble(Console.ReadLine());
     Console.Write("Задайте значение k2: ");
     double k2 = Convert.ToDouble(Console.ReadLine());
+
     double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
-    // double res1 = k1 * x + b1;
-    // double res2 = k2 * x + b2;
-    Console.WriteLine($"{x} : {y}");
+    double y1 = k1 * x + b1;
+    double y2 = k2 * x + b2;
+
+    Console.WriteLine($"({Math.Round(x, 2)} : {Math.Round(y2, 2)})");
     Console.WriteLine("Попробуем ещё раз? y/n");
     run = Console.ReadKey().Key == ConsoleKey.Y;
 }
